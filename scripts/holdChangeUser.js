8b5e5d55ad086058d3d96fc4e21527d5f55af353
thatisdrtruman <joe.liu2nd@gmail.com>
2023-02-12 19:40:46 +0000

    added sample bespoke item and holding JavaScript verification

---
+++ scripts/holdChangeUser.js
@@ -0,0 +1,42 @@
+class PressAndHold{
+    /**
+     * 
+     * @param {EventTarget} target the HTML element to apply events to
+     * @param {Function} callback function to run once the target is pressed and held
+     */
+    constructor(target, callback){
+        this.target = target;
+        this.callback = callback;
+        this.isHeld = false;
+        this.activeHoldTimeoutId = null;
+
+        ["mousedown", "touchstart"].forEach(type => {
+            this.target.addEventListener(type, this._onHoldStart.bind(this));
+        });
+
+        ["mouseup", "mouseleave", "mouseout", "touchend", "touchcancel"].forEach(type => {
+            this.target.addEventListener(type, this._onHoldEnd.bind(this));
+        });
+    }
+
+    _onHoldStart() {
+        this.isHeld = true;
+
+        this.activeHoldTimeoutId = setTimeout(() => {
+            if (this.isHeld) {
+                this.callback();
+            }
+        }, 3000); /** for 3000ms or 3 seconds */
+    }
+
+    _onHoldEnd() {
+        this.isHeld = false;
+        clearTimeout(this.activeHoldTimeoutId);
+    }
+}
+
+const holdButtonChangeUser = document.getElementById("holdButtonChangeUser");
+
+new PressAndHold(holdButtonChangeUser, () => {
+    window.location.href = 'change-person.html';
+})
